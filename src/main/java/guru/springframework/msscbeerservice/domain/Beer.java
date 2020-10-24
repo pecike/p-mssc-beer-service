885ac5fa4fd1ffbc694910bd550934dc4fe41581
Peter Cirbes <p.cirbes@gmail.com>
2020-10-25 00:18:41 +0200

    Adding Beer entity and repository.

---
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -0,0 +1,48 @@
+package guru.springframework.msscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * https://thorben-janssen.com/lombok-hibernate-how-to-avoid-common-pitfalls/
+ * https://thorben-janssen.com/generate-uuids-primary-keys-hibernate/
+ * https://thorben-janssen.com/map-date-time-api-jpa-2-2/
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy =  "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
