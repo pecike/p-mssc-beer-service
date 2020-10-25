f5a38d9ea410b48975ec01d311d376b9c5b3dc7f
Peter Cirbes <p.cirbes@gmail.com>
2020-10-25 23:21:34 +0100

    Adding tests to cover validation.

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -42,6 +44,20 @@ class BeerControllerTest {
     }
 
     @Test
+    void saveNewBeer_shouldReturnBadRequest_whenBeerDtoIsInvalid() throws Exception {
+        final BeerDto beerDto = getBasicBeerDto()
+                .beerName(" ")
+                .build();
+        final String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post(BeerController.API_V1_BEER)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.beerName", is("must not be blank")));
+    }
+
+    @Test
     void updateBeer() throws Exception {
         final UUID beerId = UUID.randomUUID();
         final BeerDto beerDto = getBasicBeerDto().build();
@@ -51,6 +67,21 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void updateBeer_shouldReturnBadRequest_whenBeerDtoIsInvalid() throws Exception {
+        final UUID beerId = UUID.randomUUID();
+        final BeerDto beerDto = getBasicBeerDto()
+                .beerName(" ")
+                .build();
+        final String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(put(BeerController.API_V1_BEER + "/" + beerId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.beerName", is("must not be blank")));
     }
 
     private BeerDto.BeerDtoBuilder getBasicBeerDto() {
